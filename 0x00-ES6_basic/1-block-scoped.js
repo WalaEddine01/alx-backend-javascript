0d0cdc56fed32e4697dda99a228f58774efbad30
walaeddine01 <walaaeddine33@gmail.com>
2024-04-10 13:36:09 +0200

    I made the changes on 2024-04-10 13:36:09

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,14 +1,13 @@
 export default function taskBlock (trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  var task = false;
+  var task2 = true;
 
-  /* eslint-disable*/
+
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    let task = true;
+    var task2 = false;
   }
 
-  /* eslint-enable no-unused-vars */
-  
+
   return [task, task2];
 }
