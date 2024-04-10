09e1cb80eb3c44e6054df455e29ddf04cec87cb9
walaeddine01 <walaaeddine33@gmail.com>
2024-04-10 13:38:09 +0200

    I made the changes on 2024-04-10 13:38:08

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,11 @@
 export default function taskBlock (trueOrFalse) {
-  var task = false;
+  const task = false;
   var task2 = true;
 
-
   if (trueOrFalse) {
-    let task = true;
+    const task = true;
     var task2 = false;
   }
-
 
   return [task, task2];
 }
