ca0dad4b031705161213382975ce07be0e1ecb65
walaeddine01 <walaaeddine33@gmail.com>
2024-04-10 23:24:07 +0200

    I made the changes on 2024-04-10 23:24:07

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,4 +1,4 @@
-export default function GetNeighborhoodsList () {
+export default function GetNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
