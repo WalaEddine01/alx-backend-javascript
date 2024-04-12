2b45eb9b7c5a9704ce0b4e19943e7c8296703a74
walaeddine01 <walaaeddine33@gmail.com>
2024-04-12 23:19:05 +0200

    I made the changes on 2024-04-12 23:19:05

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,11 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof (name) === 'string') this._name = name;
+    else throw new TypeError('Name must be a string');
     if (typeof (length) === 'number') this._length = length;
+    else throw new TypeError('Name must be a number');
     if (Array.isArray(students)) this._students = [...students];
+    else throw new TypeError('Name must be an array');
   }
 
   set name(name) {
