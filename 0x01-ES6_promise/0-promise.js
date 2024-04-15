0e8a586db5d3303df507bbc1b68e736d4b157e88
walaeddine01 <walaaeddine33@gmail.com>
2024-04-15 08:39:42 +0200

    I made the changes on 2024-04-15 08:39:42

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,19 +1,3 @@
-function isEvenNumber(num) {
-    return new Promise((resolve, reject) => {
-      if (typeof num === 'number') {
-        if (num % 2 === 0) {
-          resolve('Even');
-        } else {
-          resolve('Odd');
-        }
-      } else {
-        reject('Error: Not a number');
-      }
-    });
-  }
-  
-  isEvenNumber("d").then(
-    (resolve) => console.log(resolve),
-    (reject) => console.log(reject)
-  );
-  
+export default function getResponseFromAPI(){
+  return new Promise;
+}
