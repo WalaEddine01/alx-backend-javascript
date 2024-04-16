3357d7b466d0a7b0623d986b6621c1849da7a5a4
walaeddine01 <walaaeddine33@gmail.com>
2024-04-16 19:13:02 +0200

    I made the changes on 2024-04-16 19:13:02

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,3 +1,8 @@
 export default function getFullResponseFromAPI(success) {
-  if (success) return new Promise((r, re) => {});
+  return new Promise((r, re) => {
+    if (success) r({ status: 200, body: 'Success' });
+    else {
+      re(new Error('The fake API is not working currently'));
+    }
+  });
 }
