6efc126b278422fe10455ab25d54d23a73d196f2
walaeddine01 <walaaeddine33@gmail.com>
2024-04-20 11:50:09 +0200

    I made the changes on 2024-04-20 11:50:09

--- 0x03-ES6_data_manipulation/main.js
+++ 0x03-ES6_data_manipulation/main.js
@@ -1,8 +1,6 @@
-import getListStudentIds from "./1-get_list_student_ids.js";
 import getListStudents from "./0-get_list_students.js";
+import getStudentsByLocation from "./2-get_students_by_loc.js";
 
-console.log(getListStudentIds("hello"));
-console.log(getListStudentIds(getListStudents()));
-console.log(getListStudents().length);
-console.log(getListStudentIds(null));
-console.log(getListStudentIds('TEST'));
+const students = getListStudents();
+
+console.log(getStudentsByLocation(students, 'San Francisco'));
