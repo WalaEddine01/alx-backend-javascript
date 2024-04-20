7f2a2c2a1256e872a43a5852ea144b58affe1c5f
walaeddine01 <walaaeddine33@gmail.com>
2024-04-20 07:31:36 +0200

    I made the changes on 2024-04-20 07:31:36

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,13 +1,13 @@
 export default function getListStudents() {
-    return [
+  return [
     {
-        id:1, firstName: "Guillaume", location: "San Francisco"
+      id: 1, firstName: 'Guillaume', location: 'San Francisco',
     },
     {
-        id:2, firstName: "James", location: "Columbia"
+      id: 2, firstName: 'James', location: 'Columbia',
     },
     {
-        id:3, firstName: "Serena", location: "San Francisco"
-    }
-]
+      id: 3, firstName: 'Serena', location: 'San Francisco',
+    },
+  ];
 }
