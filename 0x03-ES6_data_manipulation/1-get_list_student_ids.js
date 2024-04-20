2715fe12bec193dfb1f201c0a840fd082a8098ca
walaeddine01 <walaaeddine33@gmail.com>
2024-04-20 11:39:53 +0200

    I made the changes on 2024-04-20 11:39:53

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,11 +1,6 @@
 export default function getListStudentIds(arg) {
-  const id = [];
   if (typeof arg !== 'object') {
-    return id;
+    return [];
   }
-  for (const a of arg) {
-    id.push(a.id);
-  }
-
-  return id;
+  return arg.map(arg => arg.id);
 }
