816d10e7a950f680a3a7c8198851c9eb1e3c7082
walaeddine01 <walaaeddine33@gmail.com>
2024-04-21 19:41:25 +0200

    I made the changes on 2024-04-21 19:41:25

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,13 +1,14 @@
 export default class Car {
-    constructor(brand, motor, color) {
-        if (typeof brand === 'string' && typeof motor === 'string' && typeof color === 'string') {
-            this._brand = brand;
-            this._motor = motor;
-            this._color = color;
-        }
+  constructor(brand, motor, color) {
+    if (typeof brand === 'string' && typeof motor === 'string' && typeof color === 'string') {
+      this._brand = brand;
+      this._motor = motor;
+      this._color = color;
     }
-    // using symbols
-    cloneCar() {
-        return Object.assign(Object.create(Object.getPrototypeOf(this)), this);
-    }
+  }
+
+  // using symbols
+  cloneCar() {
+    return Object.assign(Object.create(Object.getPrototypeOf(this)), this);
+  }
 }
