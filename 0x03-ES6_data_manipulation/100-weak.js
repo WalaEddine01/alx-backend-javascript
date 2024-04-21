947286bb5b26737c9649d561846a916e4efdc50e
walaeddine01 <walaaeddine33@gmail.com>
2024-04-21 20:11:03 +0200

    I made the changes on 2024-04-21 20:11:03

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,21 +1,13 @@
-const createAPIManager = () => {
-  const weakMap = new WeakMap();
+export const weakMap = new WeakMap();
 
-  const queryAPI = (endpoint) => {
-    if (weakMap.has(endpoint)) {
-      const endpointItem = weakMap.get(endpoint);
-      if (endpointItem >= 4) {
-        throw new Error('Endpoint load is high');
-      }
-      weakMap.set(endpoint, endpointItem + 1);
-    } else {
-      weakMap.set(endpoint, 1);
+export const queryAPI = (endpoint) => {
+  if (weakMap.has(endpoint)) {
+    const endpointItem = weakMap.get(endpoint);
+    if (endpointItem >= 4) {
+      throw new Error('Endpoint load is high');
     }
-  };
-
-  return {
-    queryAPI,
-  };
+    weakMap.set(endpoint, endpointItem + 1);
+  } else {
+    weakMap.set(endpoint, 1);
+  }
 };
-
-export default createAPIManager();
