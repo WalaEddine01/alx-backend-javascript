c8a6769930756b44b608411afeaf97c518e700f6
walaeddine01 <walaaeddine33@gmail.com>
2024-04-21 19:23:16 +0200

    I made the changes on 2024-04-21 19:23:16

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,21 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(floors) {
+    if (typeof floors === 'number') this._floors = floors;
+    else throw new TypeError('Floors must be a number');
+  }
+
+  evacuationWarningMessage() {
+    return 'Evacuate slowly the NUMBER_OF_FLOORS floors';
+  }
+}
