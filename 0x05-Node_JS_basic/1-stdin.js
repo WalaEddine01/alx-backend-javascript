2907425358ecfb4f22de94c953d28eda995b5faa
walaeddine01 <walaaeddine33@gmail.com>
2024-06-03 15:00:01 +0200

    I made the changes on 2024-06-03 15:00:01

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,6 @@
+const prompt = require('prompt-sync')();
+
+console.log("Welcome to Holberton School, what is your name?");
+const input = prompt();
+console.log(`Your name is: ${input}`);
+console.log('This important software is now closing');
