018cf9ec9ea78ea79a7ef2be395a596a26f9e2de
walaeddine01 <walaaeddine33@gmail.com>
2024-06-07 22:16:16 +0200

    I made the changes on 2024-06-07 22:16:16

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -10,8 +10,11 @@ describe("calculateNumber", function(){
     });
     it("should return 5 when passed 1.2 and 3.7", function(){
         assert.equal(calculateNumber(1.2, 3.7), 5)
-    })
+    });
     it("should return 5 when passed 1.5 and 3.7", function(){
         assert.equal(calculateNumber(1.5, 3.7), 6)
-    })
+    });
+    it('negative return -5', () => {
+        assert.equal(calculateNumber(-1.3, -3.7), -5);
+    });
 });
