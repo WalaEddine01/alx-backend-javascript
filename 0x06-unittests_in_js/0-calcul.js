70ee715f5d0c17e471f324231dac2cb8ef8b7582
walaeddine01 <walaaeddine33@gmail.com>
2024-06-07 20:41:12 +0200

    I made the changes on 2024-06-07 20:41:12

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,9 +1,5 @@
-module.exports = function calculateNumber(a, b = 0) {
-    const j = Number(b);
-    const i = Number(a);
-  
-    if (Number.isNaN(i) || Number.isNaN(j))
-      throw TypeError('Parameters must be numbers');
-  
-    return Math.round(i) + Math.round(j);
-  };
+const calculateNumber = (a, b) => {
+    return Math.round(a) + Math.round(b);
+}
+
+module.exports = calculateNumber;
