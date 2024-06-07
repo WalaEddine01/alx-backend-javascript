1a73eae4bdc18185116386dd631b8ec6d3a430d8
walaeddine01 <walaaeddine33@gmail.com>
2024-06-07 20:09:51 +0200

    I made the changes on 2024-06-07 20:09:51

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,16 +2,16 @@ const assert = require("assert");
 const calculateNumber = require("./0-calcul");
 
 describe("calculateNumber", function(){
-    it("should return 3 when passed 1 and 2", function(){
-        assert.equal(calculateNumber(1, 2), 3)
+    it("should return 4 when passed 1 and 3", function(){
+        assert.equal(calculateNumber(1, 3), 4)
     });
-    it("should return 3 when passed 1.3 and 2.3", function(){
-        assert.equal(calculateNumber(1, 2), 3)
+    it("should return 5 when passed 1 and 3.7", function(){
+        assert.equal(calculateNumber(1, 3.7), 5)
     });
-    it("should return 4 when passed 1.5 and 2", function(){
-        assert.equal(calculateNumber(1, 2), 3)
+    it("should return 5 when passed 1.2 and 3.7", function(){
+        assert.equal(calculateNumber(1.2, 3.7), 5)
     })
-    it("should return 5 when passed 1.5 and 2.6", function(){
-        assert.equal(calculateNumber(1, 2), 3)
+    it("should return 5 when passed 1.5 and 3.7", function(){
+        assert.equal(calculateNumber(1.5, 3.7), 6)
     })
 });
