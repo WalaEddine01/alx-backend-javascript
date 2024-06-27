0af4aa7046a92e0bf3663981b448da1f918599f3
walaeddine01 <walaaeddine33@gmail.com>
2024-06-27 14:45:08 +0100

    I made the changes on 2024-06-27 14:45:08

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,20 @@
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./4-payment.js');
+const Utils = require('./utils.js');
+
+
+describe('sendPaymentRequestToApi', () => {
+  it('should use Utils.calculateNumber', () => {
+    const stub = sinon.stub(Utils, 'calculateNumber');
+    stub.returns(10);
+    const spy = sinon.spy(console, 'log');
+
+    sendPaymentRequestToApi(100, 20);
+
+    sinon.assert.calledWith(stub, 'SUM', 100, 20);
+    sinon.assert.calledWith(spy, 'The total is: 10');
+
+    stub.restore();
+    spy.restore();
+  });
+});
