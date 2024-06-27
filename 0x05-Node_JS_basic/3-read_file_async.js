ac04953fa18bed5d569c48620ba2c4735f7719fb
walaeddine01 <walaaeddine33@gmail.com>
2024-06-27 12:31:15 +0100

    I made the changes on 2024-06-27 12:31:15

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -2,8 +2,8 @@ const { readFile } = require('fs');
 
 function countStudents(fileName) {
   const students = {};
-  let length = 0;
   const fields = {};
+  let length = 0;
   return new Promise((resolve, reject) => {
     readFile(fileName, (error, data) => {
       if (error) {
@@ -38,4 +38,5 @@ function countStudents(fileName) {
     });
   });
 }
+
 module.exports = countStudents;
